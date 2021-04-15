61162767022d21bc229ab512772ed4f799403b41
GustavoHen12 <gustavobarbosa@alunos.utfpr.edu.br>
2021-04-14 21:30:44 -0300

    pagina pronta

--- classeTex/settings.py
+++ classeTex/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -107,6 +108,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"), 
+]
 
 try:
     from classeTex.settings_local import *
